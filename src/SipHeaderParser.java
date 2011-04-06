0b7d847cea074187326efe32c9b35ca9e349aea2
Nauman Badar <nauman.gwt@gmail.com>
2011-04-06 16:39:05 +0200

    initial setup

--- src/SipHeaderParser.java
+++ src/SipHeaderParser.java
@@ -1,46 +1,16 @@
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import org.apache.log4j.Logger;
 
+/**
+ * @author Nauman Badar
+ * @created 6 Apr 2011
+ */
 public class SipHeaderParser {
 
+	private final static Logger log = Logger.getLogger(SipHeaderParser.class.getName());
+
 	public static void main(String[] args) {
-
-		System.out.println(loadTraceString());
-		Properties prop = new Properties();
-		try {
-			prop.load(new FileInputStream("/home/v10/naumanb/workspace/SIP Listener/sip.properties"));
-			System.out.println(prop.getProperty("tracefilepath"));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		log.info(TraceLoader.loadTraceString());
+		System.out.print(TraceLoader.loadTraceString());
 	}
 
-	private static String loadTraceString() {
-		String traceString = null;
-		try {
-			File file;
-			FileReader fr = new FileReader(file = new File("/home/v10/naumanb/trace"));
-			char charbuffer[] = new char[(int) file.length()];
-			fr.read(charbuffer);
-			traceString = new String(charbuffer);
-			traceString=traceString.trim();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	return traceString;
-	}
 }
---
+++ src/TraceLoader.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author Nauman Badar
+ * @created 6 Apr 2011
+ */
+public class TraceLoader {
+	private final static Logger log = Logger.getLogger(TraceLoader.class.getName());
+
+	static String loadTraceString() {
+
+		String traceString = null;
+		try {
+			Properties prop = new Properties();
+			prop.load(new FileInputStream("/home/v10/naumanb/workspace/SIP Listener/sip.properties"));
+			String traceFilePath = prop.getProperty("tracefilepath");
+			File file;
+			FileReader fr = new FileReader(file = new File(traceFilePath));
+			char charbuffer[] = new char[(int) file.length()];
+			fr.read(charbuffer);
+			traceString = new String(charbuffer);
+			traceString = traceString.trim();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return traceString;
+	}
+}
